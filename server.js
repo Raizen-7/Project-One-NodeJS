5c4164593f5fa8bc5cdd36e824a03e3dc1a2cdc9
Fermin Yanes <ferminyanes07@gmail.com>
2022-09-20 00:02:16 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv');
+
+const { app } = require('./app');
+
+// Utils
+
+const { db } = require('./utils/database.util');
+
+dotenv.config({ path: './config.env' });
+
+const startServer = async () => {
+	try {
+		await db.authenticate();
+
+		// Establish the relations between models
+
+
+		await db.sync();
+
+		// Set server to listen
+		const PORT = 5000;
+
+		app.listen(PORT, () => {
+			console.log('Express app ready to the party!');
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+startServer();
